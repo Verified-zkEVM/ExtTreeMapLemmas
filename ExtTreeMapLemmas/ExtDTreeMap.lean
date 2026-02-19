005d017f5e94c40b26fcc761b166dc904305bdf5
Derek Sorensen <d@dhsorens.com>
2026-02-19 10:59:57 +0000

    feat: update to 4_28

--- ExtTreeMapLemmas/ExtDTreeMap.lean
+++ ExtTreeMapLemmas/ExtDTreeMap.lean
@@ -8,6 +8,6 @@ attribute [local instance low] beqOfOrd
 theorem ExtDTreeMap.get?_filter_with_getKey_pfilter {cmp : α → α → Ordering} [TransCmp cmp]
     (m : ExtDTreeMap α (fun _ => β) cmp) (f : α → β → Bool) (k : α) :
     Const.get? (m.filter f) k = (Const.get? m k).pfilter (fun v h' => f (m.getKey k (Const.contains_eq_isSome_get?.trans (Option.isSome_of_eq_some h'))) v) := by
-  exact?
+  exact Const.get?_filter
 
 end Std
