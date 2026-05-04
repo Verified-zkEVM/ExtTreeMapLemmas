3fee686227f18dca03bb7fc42ca5a9275d6cfda6
Derek Sorensen <d@dhsorens.com>
2026-05-04 19:23:08 +0100

    feat: update to 4.29.1 (#8)

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open System Lake DSL
 
 package ExtTreeMapLemmas where version := v!"0.1.0"
 
-require "leanprover-community" / mathlib @ git "v4.29.0"
+require "leanprover-community" / mathlib @ git "v4.29.1"
 
 @[default_target]
 lean_lib ExtTreeMapLemmas
